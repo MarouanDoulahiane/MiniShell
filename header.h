ef57989aff2d880f2493fbde46f7e38885f4738a
Marouan Doulahiane <mdoulahi@e3r9p8.1337.ma>
2023-12-02 19:23:45 +0100

    setting up the prompt

---
+++ header.h
@@ -0,0 +1,30 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# define GREEN "\033[1;32m"
+# define RED "\033[1;31m"
+# define BLUE "\033[1;34m"
+# define YELLOW "\033[1;33m"
+# define CYAN "\033[1;36m"
+# define MAGENTA "\033[1;35m"
+# define WHITE "\033[1;37m"
+# define BLACK "\033[1;30m"
+# define BOLD "\033[1m"
+# define UNDERLINE "\033[4m"
+# define BLINK "\033[5m"
+# define REVERSE "\033[7m"
+# define HIDDEN "\033[8m"
+# define BGBLACK "\033[40m"
+# define BGRED "\033[41m"
+# define RESET "\033[0m"
+
+#endif
---
+++ main.c
@@ -0,0 +1,55 @@
+#include "header.h"
+#include <stdarg.h>
+
+va_list args;
+
+char	*prompt(void)
+{
+	char	*input;
+	char	path[1024];
+
+	getcwd(path, sizeof(path));
+	printf("%s%s %s %s: %s %s\n", REVERSE, RED, getenv("USER"), WHITE, path, RESET);
+	input = readline(CYAN " >_ minishell 👽 " RESET);
+	return (input);
+}
+
+bool	is_whitespace(char *input)
+{
+	int	i;
+
+	i = 0;
+	while (input[i])
+	{
+		if (input[i] != ' ' && !(input[i] >= 9 && input[i] <= 13))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	save_history(char *input)
+{
+	if (input && *input && !is_whitespace(input))
+		add_history(input);
+	free(input);
+}
+
+
+
+void	f(){system("leaks a.out");}
+
+int	main(void)
+{
+	char	*input;
+
+	// atexit(f);
+	while (1)
+	{
+		input = prompt();
+		
+		save_history(input);
+	}
+	return (0);
+}
+		
