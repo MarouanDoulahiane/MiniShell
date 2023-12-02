3bac6146d43050ba6f43bac6d123b7782af83c06
Marouan Doulahiane <mdoulahi@e3r9p8.1337.ma>
2023-12-02 21:23:10 +0100

    push

--- source/main.c
+++ source/main.c
@@ -66,7 +66,6 @@ void	handle_command(char **command, char **env)
 	if (ft_strcmp(command[0], "exit") == 0)
 	{
 		free_array(command);
-		
 		exit(0);
 	}
 	// else if (ft_strcmp(command[0], "echo") == 0)
@@ -97,8 +96,8 @@ int	main(int ac, char **av, char **env)
 	{
 		input = prompt();
 		command = ft_split(input, ' ');
-		handle_command(command, env);
 		save_history(input);
+		handle_command(command, env);
 	}
 	return (0);
 }
